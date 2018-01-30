5318e90e381ef0df01e30b4d9ada93d2fe31d363
malloryporhce <malloryburke@gmail.com>
2018-01-30 10:25:54 -0800

    Initial commit, added bodyParser

---
+++ app.js
@@ -0,0 +1,63 @@
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const objects = require('./objects.js');
+
+const app = express();
+
+const colors = [
+  'red',
+  'orange',
+  'yellow',
+  'green',
+  'blue',
+  'purple'
+];
+
+const friends = [
+  {
+      firstName: 'Mallory',
+      lastName: 'Burke'
+  },
+  {
+      firstName: 'Sylvia',
+      lastName: 'Neil'
+  },
+  {
+      firstName: 'Cameron',
+      lastName: 'Phillips'
+  },
+  {
+      firstName: 'Ralph',
+      lastName: 'Porche'
+  },
+];
+
+app.use(bodyParser.urlencoded({ extended: false}));
+
+app.set('view engine','pug');
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.get('/cards', (req, res) => {
+  res.render('card', {prompt: "Who is buried in Grant's tomb?", hint:
+ "Think about who's tomb it is", colors});
+});
+
+app.get('/sandbox', (req, res) => {
+  res.render('sandbox', {friends});
+});
+
+app.get('/hello', (req, res) => {
+  res.render('hello', {});
+});
+app.post('/hello', (req, res) => {
+  res.render('hello', {});
+  console.log(res)
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on localhost:3000!');
+});
---
+++ objects.js
@@ -0,0 +1,27 @@
+const colors = [
+  'red',
+  'orange',
+  'yellow',
+  'green',
+  'blue',
+  'purple'
+];
+
+const friends = [
+  {
+      firstName: 'Mallory',
+      lastName: 'Burke'
+  },
+  {
+      firstName: 'Sylvia',
+      lastName: 'Neil'
+  },
+  {
+      firstName: 'Cameron',
+      lastName: 'Phillips'
+  },
+  {
+      firstName: 'Ralph',
+      lastName: 'Porche'
+  },
+];
