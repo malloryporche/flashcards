3b0ae0a0b5a2829d5d7ca51b43b98ed75533d77a
malloryporhce <malloryburke@gmail.com>
2018-02-08 11:50:05 -0800

    Added link to view next card

--- app.js
+++ app.js
@@ -21,9 +21,10 @@ app.use((req, res, next) => {
   err.status = 404;
   next(err);
 });
+
 app.use((err, req, res, next) => {
   res.locals.error = err;
-  // res.status(err.status);
+  res.status(err.status);
   res.render('error');
 });
 
--- routes/cards.js
+++ routes/cards.js
@@ -3,16 +3,32 @@ const router = express.Router();
 const { data }  = require('../data/flashcardData');
 const { cards } = data;
 
+router.get('/', ( req, res) => {
+  const numberOfCards = cards.length;
+  const flashcardId = Math.floor( Math.random() * numberOfCards );
+  res.redirect(`/cards/${flashcardId}`);
+});
+
 router.get('/:id', (req, res) => {
   const { side } = req.query;
-  const { id } = req. params;
+  const { id } = req.params;
+
+  if ( !side ) {
+    res.redirect(`/cards/${id}?side=question`);
+  }
+
   const text = cards[id][side];
   const { hint } = cards[id];
 
-  const templateData = {text};
+  const templateData = {id, text};
 
   if (side === 'question' ) {
     templateData.hint = hint;
+    templateData.sideToShow = 'answer';
+    templateData.sideToShowDisplay = 'Answer';
+  } else if (side === 'answer') {
+    templateData.sideToShow = 'question';
+    templateData.sideToShowDisplay = 'Question';
   }
 
   res.render('card', templateData);
