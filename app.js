15b397ee81374305b1f167e7b3b7a579dad7aa93
malloryporhce <malloryburke@gmail.com>
2018-02-07 19:13:38 -0800

    Added error handling middleware

--- app.js
+++ app.js
@@ -2,7 +2,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-const objects = require('./objects.js');
 
 const app = express();
 
@@ -39,6 +38,19 @@ app.use(cookieParser());
 
 app.set('view engine','pug');
 
+app.use((req, res, next) => {
+  console.log('One');
+  const err = new Error('Oh noes!');
+  err.status = 500;
+  next(err);
+});
+app.use((req, res, next) => {
+  console.log('Two');
+  next();
+});
+
+
+
 app.get('/', (req, res) => {
   const name = req.cookies.username;
   if (name) {
@@ -58,11 +70,29 @@ app.get('/sandbox', (req, res) => {
 });
 
 app.get('/hello', (req, res) => {
-  res.render('hello', { name: req.cookies.username});
+  const name = req.cookies.username;
+  if (name) {
+    res.redirect('/');
+  } else {
+    res.render('hello');
+  }
 });
+
 app.post('/hello', (req, res) => {
   res.cookie('username', req.body.username );
   res.redirect('/');
+});
+
+app.post('/goodbye', (req, res) => {
+  console.log(req.body.username);
+  res.clearCookie('username');
+  res.redirect('/hello');
+});
+
+app.use((err, req, res, next) => {
+  res.locals.error = err;
+  res.status(500);
+  res.render('error');
 });
 
 app.listen(3000, () => {
